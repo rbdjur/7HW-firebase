678e5aa84454991d0b832bbd8a1b4c432dddbcf2
Ronald Deguzman <bootcampcoding.ronnie@gmail.com>
2018-04-11 00:22:19 -0700

    fixed bugs in displaying time, arrival, frequency, etc.

--- javascript/app.js
+++ javascript/app.js
@@ -9,15 +9,15 @@
 // 1. Initialize firebase
 // declare variable with firebase information (javascript)
 var config = {
-    apiKey: "AIzaSyDB2GXeY6DhUDzpitlLuaDaO0GTgQQI3FI",
-    authDomain: "homework-7-d216f.firebaseapp.com",
-    databaseURL: "https://homework-7-d216f.firebaseio.com",
-    projectId: "homework-7-d216f",
-    storageBucket: "homework-7-d216f.appspot.com",
-    messagingSenderId: "954744625559"
-};
-
-firebase.initializeApp(config);
+    apiKey: "AIzaSyDg9drSeRVEzCw0QwOz3rfOECJc377zWdY",
+    authDomain: "hw-7-use-this-one.firebaseapp.com",
+    databaseURL: "https://hw-7-use-this-one.firebaseio.com",
+    projectId: "hw-7-use-this-one",
+    storageBucket: "hw-7-use-this-one.appspot.com",
+    messagingSenderId: "420946593445"
+  };
+  firebase.initializeApp(config);
+// console.log(config);
 
 // variable referencing database. 
 var database = firebase.database();
@@ -35,17 +35,61 @@ var firstTrain = $('#firstTrainInput').val();
 var frequency = $('#frequencyInput').val();
 var currentTime = $('#currentTimeInput').val();
 
-// Define current time (hours + minutes) and place in variable 
-var hourNow = moment().hour();
-var minuteNow = moment().minute();
-$('#currentTimeInput').val(hourNow + " " + ":" + " " + minuteNow);
-console.log("Working?");
+// Train comes every five minutes
+var tFrequency = 5;
 
-// Trains run every 15 minutes 
-var nextTrain = moment().add(15, "minutes");
-var nextTrainHour = moment(nextTrain).hour();
-var nextTrainMinute = moment(nextTrain).minute();
-console.log(nextTrainHour, nextTrainMinute);
+// Time is 3:30 AM
+var firstTime = "3:30";
+
+// First time (pushed back 1 year so time conflicts do not occur)
+var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+console.log(firstTimeConverted);
+
+// Current Time
+var timeNow = moment();
+console.log("CURRENT TIME: " + moment(timeNow).format("hh:mm"));
+
+// Difference between the times
+var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+console.log("DIFFERENCE IN TIME: " + diffTime);
+
+// Time apart (remainder)
+var tRemainder = diffTime % tFrequency;
+console.log(tRemainder);
+
+// Minute Until Train
+var tMinutesTillTrain = tFrequency - tRemainder;
+console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+ // Next Train
+ var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+ console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+
+
+
+
+
+// TIME SECTION
+
+// // Define current time (hours + minutes) and place in variable 
+// var hourNow = moment().hour();
+// var minuteNow = moment().minute();
+// $('#currentTimeInput').val(hourNow + " " + ":" + " " + minuteNow);
+// console.log("Working?");
+
+// // Trains run every 15 minutes 
+// var nextTrain = moment().add(15, "minutes");
+// var nextTrainHour = moment(nextTrain).hour();
+// var nextTrainMinute = moment(nextTrain).minute();
+// console.log(nextTrainHour, nextTrainMinute);
+
+// Difference between times
+
+// var diffTime = moment().diff(moment())
+
+// Minutes Away Column
+// - Do we use firebase to randomly generate numbers on the back-end side to populate this column when called upon?
 
 // SECOND create new row
 var newRow = $("<tr>");
@@ -53,9 +97,13 @@ var newRow = $("<tr>");
 newRow.append("<td>" + trainName + "</td>");
 newRow.append("<td>" + destination + "</td>");
 newRow.append("<td>" + frequency + "</td>");
-newRow.append("<td>" + nextTrainHour + " " + " : " + " " + nextTrainMinute + " " + "</td>");
-newRow.append("<td>" + " " + "</td>");
-newRow.append("<td>" + hourNow + " " + " : " + " " + minuteNow + " " + "</td>");
+newRow.append("<td>" + moment(nextTrain).format("hh:mm") + "</td>");
+// newRow.append("<td>" + nextTrainHour + " " + " : " + " " + nextTrainMinute + " " + "</td>");
+newRow.append("<td>" + tMinutesTillTrain + "</td>");
+// newRow.append("<td>" + hourNow + " " + " : " + " " + minuteNow + " " + "</td>");
+newRow.append("<td>" + timeNow.format("hh:mm") + "</td>");
+
+
 
 // //Not appending in correct spot
 // newRow.append("<td>" + timeNow + "</td>");
@@ -70,7 +118,7 @@ var newTrain = {
     wait: frequency, 
     // time: currentTime
 };
-console.log(newTrain);
+console.log(newTrain.nameOfTrain);
 
 // Uploads employee data to the database
 // Access issues communicating with firebase. 
@@ -94,9 +142,15 @@ $('#currentTimeInput').val("");
 });
 
 // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
-// This line of code at 75 shows error that failed: permission_denied
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {
     console.log(childSnapshot.val());
+
+// RIGHT HERE - put the values of the form in the html, inside here.  
+var trainName = $('#trainNameInput').val();
+var destination = $('#destinationInput').val();
+var firstTrain = $('#firstTrainInput').val();
+var frequency = $('#frequencyInput').val();
+var currentTime = $('#currentTimeInput').val();
 
 // Store everything into a variable
 var trainName = childSnapshot.val().nameOfTrain;
@@ -112,19 +166,17 @@ console.log(firstTrain);
 console.log(frequency);
 // console.log(time);
 
-
-// // Display time at current time
-// var currentTime = new date();
-// console.log(currentTime);
+// Add a .set() method to add the desired information to firebase and have it persist. 
+// also refer to train example exercise and firebase exercises that allow you to change the information from the back-end side (firebase). 
 
 
 
 
 
 
-// // Prettify the train start
-// var trainStartTime = moment.unix(firstTrain).format();
-// console.log("moment js works?");
+// Prettify the train start
+var trainStartTime = moment.unix(firstTrain).format();
+console.log("moment js works?");
 
 
 // // calculate time until next arrival by adding the value of the frequency input
